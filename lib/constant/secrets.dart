eeba0a032aa518976758a45cf2781b27f8b6e8e9
Ygwill <102452085+Ygwill@users.noreply.github.com>
2026-07-31 09:43:59 +0800

    Create secrets.dart with OAuth constants
    Add OAuth and client ID constants for various services.

---
+++ lib/constant/secrets.dart
@@ -0,0 +1,9 @@
+// lib/constant/secrets.dart
+const String oauthRedirectUrl = 'http://localhost:8080/callback';
+const String gitHubClientId = 'dummy';
+const String gitHubClientSecret = 'dummy';
+const String gitHubAppClientId = 'dummy';
+const String gitHubAppClientSecret = 'dummy';
+const String giteaClientId = 'dummy';
+const String gitlabClientId = 'dummy';
+const String codebergClientId = 'dummy';
